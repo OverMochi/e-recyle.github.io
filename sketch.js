9db0e8a75d43c3fd58d9cfffe271c5a9d2c7c018
overmochi <90480510+OverMochi@users.noreply.github.com>
2021-09-16 10:51:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 var medFont, regFont, semiFont;
-var image2, image3, image4, image5;
+var image2, image3, image4, image5, image6;
 var icon1;
 var bar;
 var logo;
 function preload(){
   image2 = loadImage('takeout.png');
+  image3 = loadImage('plastic.png');
+  image4 = loadImage('glass.png');
+  image5 = loadImage('paper.png');
+  image6 = loadImage('batteries.png');
   icon1 = loadImage('menu.png');
   bar = loadImage('status_bar.png');
   logo = loadImage('e-recycle.png');
@@ -18,7 +22,7 @@ function preload(){
 
 function setup() {
   // put setup code here
-  createCanvas(375, 966);
+  createCanvas(375, 812);
   link = createA('item.html', '');
 }
 
@@ -49,12 +53,20 @@ function draw() {
   rect(198, 784, 147, 147, 15);
 
   image(image2,238,313,67,34);
+  image(image3,83,461,34,76);
+  image(image4,253,468,36,66);
+  image(image5,77,643,52,65);
+  image(image6,258,640,28,68);
 
   textSize(18)
   fill(128);
   textFont(medFont);
   text('CUPS', 78, 373,51,20);
   text('TAKE OUT', 226, 373,98,20);
+  text('PLASTIC', 65, 546,76,20);
+  text('GLASS', 242, 546,60,20);
+  text('PAPER', 72, 721,63,20);
+  text('BATTERIES', 221, 721,100,20);
 
   img = createImg('cups.png').parent(link);
   img.position(81, 279);
